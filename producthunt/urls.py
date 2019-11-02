d1c9bc192f075be8af9a550901f1ee28ec8ce6bf
Jagdish Sharma <jagdishcsharma29@gmail.com>
2019-11-02 11:10:08 +0530

    Product hunt complete

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from products import views
@@ -6,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
-]
+    path('products/', include('products.urls')),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.TextField()
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)#If user is deleted than delete this product also
+
+    def __str__(self):
+        return self.title[:100]
+
+    def summary(self):
+        return self.body[:200]
 # Create your models here.
